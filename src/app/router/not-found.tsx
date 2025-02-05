3e587a26f74dcdef102c8a6598cce064cd0ffc99
arishkaa05 <pokazatskaya02@mail.ru>
2025-02-05 22:32:35 +0300

    feat: not found page

--- src/app/router/not-found.tsx
+++ src/app/router/not-found.tsx
@@ -1,8 +1,17 @@
+import { Container } from "@/components/container";
+import { Title } from "@/components/title";
+import { Button } from "@/components/ui/button";
+
 const NotFound = () => {
   return (
-      <div>
-        Not found
+    <Container variant={'default'}>
+      <div className="min-h-[65vh] flex flex-col items-center justify-center gap-4">
+        <Title text={'Ошибка 404'} size="2xl" className="font-bold text-gray-300" />
+        <Title text={'Страницы с таким адресом не существует'} size="lg" className="font-semibold text-gray-700" />
+
+        <Button>На главную</Button>
       </div>
+    </Container>
   );
 };
 
