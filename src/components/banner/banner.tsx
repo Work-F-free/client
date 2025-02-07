3140e98679ef0eab7477aad3815d20fa5009b358
arishkaa05 <pokazatskaya02@mail.ru>
2025-02-07 18:09:23 +0300

    add: coworking link

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -10,7 +10,7 @@ export const Banner: FC = () => {
   return (
     <div className="h-[650px] md:h-[450px] relative overflow-hidden rounded">
       <Geolocation className='absolute z-10 m-1 sm:m-6' />
-      <img className="w-full object-cover object-center h-full absolute top-0 " src='/banner-bg.png' alt='banner' />
+      <img className="w-full object-cover object-center h-full absolute top-0 " src='/banner-bg.png' alt='Koworka' />
       <div className='h-full absolute  w-full bg-black/30'></div>
       <Link to='/'>
         <div className='w-full flex justify-center text-[10px] py-2 font-light gap-2 absolute bottom-0 bg-black/55 text-center  cursor-pointer'>
@@ -23,7 +23,7 @@ export const Banner: FC = () => {
       </Link>
       <div className='absolute h-full w-full z-10'>
         <div className='flex flex-col p-6 gap-4 h-full items-center justify-center  '>
-          <Title text={'Если выбор коворкинга, то WORK FOR FREE '} size="lg" className="font-semibold text-center text-white" />
+          <Title text={'Если выбор коворкинга, то koworka '} size="lg" className="font-semibold text-center text-white" />
           <CoworkingMainFilter />
         </div>
       </div>
